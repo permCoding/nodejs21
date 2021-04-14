2742f1912464c6a31a6b6b83a625cd63c6892c7e
Андрей Беляков <ttxiom@gmail.com>
2021-04-14 23:57:58 +0500

    up

--- theme-06-MySQL/part2-SQL/05-count.js
+++ theme-06-MySQL/part2-SQL/05-count.js
@@ -15,7 +15,7 @@ const conn = get_conn();
 
 conn.promise()
     .query("SELECT COUNT(*) FROM abiturs")
-    .then(([rows]) => rows[0]['COUNT(*)'])
+    .then(([rows]) => { return rows[0]['COUNT(*)']; })
     .then((count) => { console.log('count =', count) })
     .catch((err) => { console.error(err) })
     .then(conn.end());
