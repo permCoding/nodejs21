42a25b553ddd7a2fb95a48c7fb7db39c24b696d8
Андрей Беляков <ttxiom@gmail.com>
2021-04-24 23:07:18 +0500

    up

--- theme-06-MySQL/part2-SQL/10-readjson.js
+++ theme-06-MySQL/part2-SQL/10-readjson.js
@@ -5,10 +5,15 @@ const _ = require('lodash');
 
 function ex_6(url) {
     fetch(url)
-        .then(resp => resp.json())
-        .then((json)=>console.table(
-            _.orderBy(json, ['rating','lastName'],['desc','asc']))
-        );
+        .then(resp => console.log(resp));
+    // .then((json)=>console.table(
+    //     _.orderBy(json, ['rating','lastName'],['desc','asc']))
+    // );
+    // fetch(url)
+    //     .then(resp => resp.json())
+    //     .then((json)=>console.table(
+    //         _.orderBy(json, ['rating','lastName'],['desc','asc']))
+    //     );
 }
 
 ex_6('https://pCoding.ru/csv/abiturs.json');
